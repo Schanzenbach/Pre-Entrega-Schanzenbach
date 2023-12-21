6fce629ab4a7da8486f3d0e364264d458f4d9636
Alan Schanzenbach <alanschanzenbach@gmail.com>
2023-12-21 01:24:42 -0300

    arreglado carrito icon

--- src/components/Kart/Kart.jsx
+++ src/components/Kart/Kart.jsx
@@ -5,8 +5,8 @@ import { BsCart4 } from "react-icons/bs";
 export const Kart = () => {
   return (
     <div className='cart-container'>
-        <div className='icon-container'><BsCart4 /></div>
-        <div className='cart-amount'>0</div>
+        <BsCart4 className='kart-icon' size="3em"/>
+        <p>0</p>         
     </div>
   )
 }
